8ff3b4eec2fa37fb8acfb0ccf5d3432a09e9906f
Sue Ceklosky <cekloskys@chc.edu>
2022-09-01 10:50:56 -0400

    Illustrated OOP programming model and strict typing

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Abstraction is to work with something we know how to use without
+        // knowing how it works internally.
+        Student student = new Student("Susan", "Ceklosky", "CHC");
+        System.out.println(student.greeting());
+
+        Integer one = 1;
+        Double two = 2.0;
+        String twoString = "2.0";
+        Boolean bool = false;
+
+        // Strongly typed programming languages will attempt to determine the type for you
+        System.out.println(one + two);
+        System.out.println(one + twoString);
+
+        // System.out.println(one + bool);
+    }
+}
